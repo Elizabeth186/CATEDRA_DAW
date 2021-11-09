4f39506c9108f5c17364c3cb25a947bc7eccf200
FernandoMerlos_25 <79458666+8888220@users.noreply.github.com>
2021-11-09 13:34:40 -0300

    cambios formulario JQuery

--- El-Salvador/src/app/components/registro/validaciones.js
+++ El-Salvador/src/app/components/registro/validaciones.js
@@ -1,29 +1,42 @@
-$(function() {
+$(function () {
   $("form[name='registration']").validate({
-  rules: {
+    rules: {
       firstname: "required",
-          lastname: "required",
-          email: {
-            required: true,
-            email: true
-          },
-          password: {
-            required: true,
-            minlength: 5
-          }
-        },
-        messages: {
-          firstname: "Por favor, introduzca su nombre",
-          lastname: "Por favor, introduzca su apellido",
-          password: {
-            required: "Por favor proporcione una contraseña",
-            minlength: "Su contraseña debe tener al menos 5 caracteres."
-          },
-          email: "Por favor, introduce una dirección de correo electrónico válida"
-        },
-        submitHandler: function(form) {  
-          form.submit();
+      lastname: "required",
+    
+      email: {
+        required: true,
+        email: true
+      },
+      password: {
+        required: true,
+        minlength: 5
+      },
+     
+    },
+    messages: {
+      firstname: "Por favor, introduzca su nombre",
+      lastname: "Por favor, introduzca su apellido",
+      password: {
+        required: "Por favor proporcione una contraseña",
+        minlength: "Su contraseña debe tener al menos 5 caracteres."
+      },
+      email: "Por favor, introduce una dirección de correo electrónico válida",
+    
+   
+    },
+    
+
   
-        }
-      });
-    });
+  
+
+    submitHandler: function (form) {
+      form.submit();
+
+    }
+  });
+});
+
+$(document).ready(function () {
+  $('.telefono').mask('0000-0000');
+})
